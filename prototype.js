23bddac4d713dd7ba4a546acaf9dac7f32064b30
demd7362 <demd7362@gmail.com>
2022-12-12 17:43:43 +0900

    xhr 연습

--- prototype.js
+++ prototype.js
@@ -2,21 +2,18 @@
  * Fisrt 클래스입니다.
  */
 
-
 class First {
-            
-    constructor(one,two){
-        this.one=one;
-        this.two=two;
+    constructor(one, two) {
+        this.one = one;
+        this.two = two;
     }
-    talk(){
-        console.log(this.one,this.two);
+    talk() {
+        console.log(this.one, this.two);
     }
 }
-var first = new First('안녕','하세요');
+var first = new First("안녕", "하세요");
 first.talk();
 
-First.prototype.name = new First('반갑','습니다');
+First.prototype.name = new First("반갑", "습니다");
 
 console.log(first.name);
-
