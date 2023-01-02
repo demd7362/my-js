4755fd003cbb10e2a37da9d286b80ccfc2963bd6
demd7362 <demd7362@gmail.com>
2023-01-03 00:03:49 +0900

    import,export

--- vue/vuedongsan/src/App.vue
+++ vue/vuedongsan/src/App.vue
@@ -1,38 +1,44 @@
-<template>
-  
+<template >
   <div class="menu">
     <a v-for="(menu) in menus" :key="menu">{{ menu }}</a>
-   
-
-  </div>
-  
-  <div v-for="item,i in products" :key="i">
-    <img :src="images[i]">
-    <h4>{{ item }}</h4>
-    <p>{{ ((i+1)*10)+'만원' }}</p>
-    <button @click="increase(i)">허위매물신고</button> <span>신고 수 : {{ cnts[i] }}</span>
   </div>
 
-  
+  <div class="black-bg" v-if="isModalOpen" >
+    <div class="white-bg" >
+      <h4>data.title</h4>
+      <p>data.content</p>
+      <button @click="isModalOpen=false">닫기</button>
+    </div>
+  </div>
+
+
+  <div v-for="a,i in onerooms" :key="i">
+    <img :src="onerooms[i].image" class="room-img">
+    <h4 @click="isModalOpen=true">{{ onerooms[i].title}}</h4>
+    <p>{{ onerooms[i].price +'원' }}</p>
+  </div>
 
 </template>
 
 <script>
+import oneroom from './assets/oneroom.js';
+
+
 
 export default {
   name: 'App',
   data() {
     return {
-      cnts : [0,0,0],
-      images : ['./assets/room0.jpg','./assets/room1.jpg','./assets/room2.jpg'],
-      menus : ['Home','Shop','About'],
+      onerooms : oneroom,
+      isModalOpen : false,
+      cnts: [0, 0, 0],
+      images: [require('@/assets/room0.jpg'), require('@/assets/room1.jpg'), require('@/assets/room2.jpg')],
+      menus: ['Home', 'Shop', 'About'],
       products: ['역삼동원룸', '천호동원룸', '마포구원룸'],
     }
   },
-  methods :{
-    increase(i){
-      this.cnts[i]++;
-    }
+  methods: {
+    
   },
   components: {
 
@@ -47,17 +53,43 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  
+
 }
 
 .menu {
   background: darkslateblue;
   padding: 15px;
-  border-radius : 5px;
+  border-radius: 5px;
 }
 
 .menu a {
-  color:white;
+  color: white;
   padding: 10px;
+}
+
+.room-img {
+  width: 100%;
+  margin-top: 40px;
+}
+
+body {
+  margin: 0;
+}
+
+div {
+  box-sizing: border-box;
+}
+
+.black-bg {
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0, 5);
+  position: fixed; padding: 20px;
+}
+
+.white-bg {
+  width: 100%; background: white;
+  border-radius:8px;
+  padding: 20px;
 }
 </style>
