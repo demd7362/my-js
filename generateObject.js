0703617f271a1d943decc3c6e5939c384cde10eb
Jihun Jeong <demd7362@gmail.com>
2023-06-19 19:59:49 +0900

    Update generateObject.js

--- generateObject.js
+++ generateObject.js
@@ -2,9 +2,9 @@ function generateObject(attribute) {
             const obj = {};
 
             document.querySelectorAll(`[${attribute}]`).forEach(el => {
-                let target = el.getAttribute(`${attribute}`);
+                let target = el.getAttribute(attribute);
                 target = target.split('.');
-                let currentObj = obj;
+                let currentObj = obj; // 현재 작업할 객체를 담을 변수 currentObj 
                 const length = target.length;
 
                 for (let i = 0; i < length; i++) {
@@ -16,14 +16,14 @@ function generateObject(attribute) {
                         if (currentObj[arrayKey] === undefined) {
                             currentObj[arrayKey] = [];
                         }
-                        currentObj = currentObj[arrayKey];
+                        currentObj = currentObj[arrayKey]; // currentObj는 배열이 됨
                         if (i === length - 1) {
-                            currentObj[index] = el.value;
+                            currentObj[index] = el.value; // ex) test1.test2[0] 일 시 test1 객체 안에 존재하는 test2[0]의 값은 el.value
                         } else {
                             if (currentObj[index] === undefined) {
                                 currentObj[index] = {};
                             }
-                            currentObj = currentObj[index];
+                            currentObj = currentObj[index]; // 다음 i에 사용할 객체를 대입
                         }
                     } else if(key.includes('[]')){ // 빈 배열
                         const arrayKey = key.split('[]')[0];
@@ -39,7 +39,7 @@ function generateObject(attribute) {
                         if (i === length - 1) {
                             currentObj[key] = el.value;
                         } else {
-                            currentObj = currentObj[key];
+                            currentObj = currentObj[key]; // 다음 i에 사용할 객체를 대입
                         }
                     }
                 }
